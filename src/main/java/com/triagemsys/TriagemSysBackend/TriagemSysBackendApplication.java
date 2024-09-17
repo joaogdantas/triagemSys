484be8548f231b60228179fe12dea5f1bf37b6a1
João Gabriel <joaogdantasdev@hotmail.com>
2024-09-17 19:07:32 -0300

    Add project basic structure

---
+++ src/main/java/com/triagemsys/TriagemSysBackend/TriagemSysBackendApplication.java
@@ -0,0 +1,13 @@
+package com.triagemsys.TriagemSysBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriagemSysBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriagemSysBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/triagemsys/TriagemSysBackend/TriagemSysBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triagemsys.TriagemSysBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriagemSysBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

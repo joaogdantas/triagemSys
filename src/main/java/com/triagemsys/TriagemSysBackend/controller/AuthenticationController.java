b8efcd6662cdf7b5978e4aa2a525b55062033e44
Joao Gabriel <joao.gabriel@esig.com.br>
2024-10-14 19:38:44 -0300

    FEAT: Adicionado controller

---
+++ src/main/java/com/triagemsys/TriagemSysBackend/controller/AuthenticationController.java
@@ -0,0 +1,47 @@
+package com.triagemsys.TriagemSysBackend.controller;
+
+import com.triagemsys.TriagemSysBackend.domain.service.security.AuthenticationService;
+import com.triagemsys.TriagemSysBackend.domain.service.security.JwtService;
+import com.triagemsys.TriagemSysBackend.domain.user.User;
+import com.triagemsys.TriagemSysBackend.domain.user.data.LoginUserDTO;
+import com.triagemsys.TriagemSysBackend.domain.user.data.RegisterUserDTO;
+import com.triagemsys.TriagemSysBackend.infra.web.LoginResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthenticationController {
+    private final JwtService jwtService;
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDTO registerUserDto) {
+        User registeredUser = authenticationService.signup(registerUserDto);
+
+        return ResponseEntity.status(HttpStatus.OK.value()).body(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDTO loginUserDto) {
+        User authenticatedUser = authenticationService.authenticate(loginUserDto);
+
+        String jwtToken = jwtService.generateToken(authenticatedUser);
+
+        LoginResponse loginResponse = new LoginResponse();
+        loginResponse.setToken(jwtToken);
+        loginResponse.setExpiresIn(jwtService.getExpirationTime());
+
+        return ResponseEntity.status(HttpStatus.OK.value()).body(loginResponse);
+    }
+}
--- src/main/java/com/triagemsys/TriagemSysBackend/controller/PatientController.java
+++ src/main/java/com/triagemsys/TriagemSysBackend/controller/PatientController.java
@@ -2,12 +2,16 @@ package com.triagemsys.TriagemSysBackend.controller;
 
 import com.triagemsys.TriagemSysBackend.domain.patient.Patient;
 import com.triagemsys.TriagemSysBackend.domain.patient.data.RegisterPatientDTO;
+import com.triagemsys.TriagemSysBackend.domain.patient.data.ReturnPatientDTO;
 import com.triagemsys.TriagemSysBackend.domain.repository.PatientRepository;
 import com.triagemsys.TriagemSysBackend.domain.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/patients")
@@ -24,6 +28,30 @@ public class PatientController {
 
         Patient registeredPatient = patientService.registerPatient(registerPatientDTO);
         return ResponseEntity.status(HttpStatus.CREATED.value()).body("Paciente registrado com sucesso!");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ReturnPatientDTO> getPatientById(@PathVariable UUID id) {
+        ReturnPatientDTO patientDTO = patientService.getPatientById(id);
+        return ResponseEntity.status(HttpStatus.FOUND.value()).body(patientDTO);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<ReturnPatientDTO>> getAllPatients() {
+        List<ReturnPatientDTO> patients = patientService.findAllPatients();
+        return ResponseEntity.status(HttpStatus.FOUND.value()).body(patients);
+    }
+
+    @PutMapping("update/{id}")
+    public ResponseEntity<String> editPatient(@PathVariable UUID id, @RequestBody RegisterPatientDTO registerPatientDTO) {
+        Patient updatedPatient = patientService.editPatient(id, registerPatientDTO);
+        return ResponseEntity.status(HttpStatus.OK.value()).body("Paciente atualizado com sucesso!");
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deletePatientById(@PathVariable UUID id) {
+        patientService.deletePatientById(id);
+        return ResponseEntity.status(HttpStatus.OK.value()).body("Paciente deletado com sucesso!");
     }
 }
 
